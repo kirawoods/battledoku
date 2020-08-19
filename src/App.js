daf356012eb5647557898e08b4f83b8438ca0caf
Kira <kawoods826@gmail.com>
2020-08-18 23:09:41 -0700

    adds buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
-import { Header } from "./components/Header";
 import { Board } from "./components/Board";
 import { Scoreboard } from "./components/Scoreboard";
 function App() {
   return (
     <div className="App">
-      <Header />
       <div className="game-container">
         <Scoreboard />
         <Board />
--- src/components/Board.js
+++ src/components/Board.js
@@ -4,7 +4,7 @@ import "./Board.css";
 export function Board() {
   return (
     <div className="Board">
-      <div className="board-header">Board</div>
+      <div className="board-header">Battledoku</div>
       <div className="game-board">
         <div className="nine-container">
           <div className="number-box"></div>
@@ -105,6 +105,17 @@ export function Board() {
           <div className="number-box"></div>
           <div className="number-box"></div>
         </div>
+      </div>
+      <div className="number-buttons-container">
+        <div className="number-button">1</div>
+        <div className="number-button">2</div>
+        <div className="number-button">3</div>
+        <div className="number-button">4</div>
+        <div className="number-button">5</div>
+        <div className="number-button">6</div>
+        <div className="number-button">7</div>
+        <div className="number-button">8</div>
+        <div className="number-button">9</div>
       </div>
     </div>
   );
