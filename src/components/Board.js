1678b002e7a75a19e91f77a8cd2a11779406e67e
Kira <kawoods826@gmail.com>
2020-08-18 22:47:01 -0700

    adds layout for gameboard

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,5 +2,110 @@ import React from "react";
 import "./Board.css";
 
 export function Board() {
-  return <div className="Board">Board</div>;
+  return (
+    <div className="Board">
+      <div className="board-header">Board</div>
+      <div className="game-board">
+        <div className="nine-container">
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+        </div>
+        <div className="nine-container">
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+        </div>
+        <div className="nine-container">
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+        </div>
+        <div className="nine-container">
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+        </div>
+        <div className="nine-container">
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+        </div>
+        <div className="nine-container">
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+        </div>
+        <div className="nine-container">
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+        </div>
+        <div className="nine-container">
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+        </div>
+        <div className="nine-container">
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+          <div className="number-box"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
